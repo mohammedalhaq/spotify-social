1efa0b058a46e1d41b5469b9d99b601265d3251e
Mohammed Alhaq <mohammed.alhaq.523@gmail.com>
2021-03-03 00:11:43 -0500

    Initial commit with backend

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        
+          <a>
+            Learn
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
