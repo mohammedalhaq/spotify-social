7bd6350948ab2d97fe053728d858b022222c4be7
Mohammed Alhaq <mohammed.alhaq.523@gmail.com>
2021-04-09 17:49:50 -0400

    design changes

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -12,9 +12,10 @@ import Tab from '@material-ui/core/Tab';
 function Home() {
   const [content, setContent] = useState([]);
   const [artists, setArtists] = useState(false);
-  const [timeRange, setTime] = React.useState("/?time_range=long_term");
+  const [timeRange, setTime] = useState("/?time_range=long_term");
+  const [header, setHeader] = useState("https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80");
 
-  const showContent = (type, time) => { //"/?t="
+  const showContent = (type, time) => { 
     const token = window.location.hash.split("access_token=")[1].split("&token_type=")[0]
     fetch("https://api.spotify.com/v1/me/top/" + type + time, {
       method: 'GET',
@@ -45,15 +46,24 @@ function Home() {
 
 
   const handleChange = (event, newValue) => {
-    const type = artists === true ? "artists" : "tracks";
     let time = "/?time_range=long_term ";
     if (event.target.outerText === "LAST 6 MONTHS") {
       time = "/?time_range=medium_term";
     } else if (event.target.outerText === "LAST MONTH") {
       time = "/?time_range=short_term";
     }
-    showContent(type, time);
     setTime(time);
+
+    if (artists=== true){
+      const type = "artists";
+      showContent(type, time);
+      setHeader("https://images.unsplash.com/photo-1576328172036-6d52156c525f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=882&q=80")
+    } else {
+      const type = "tracks";
+      showContent(type, time);
+      setHeader("https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")
+
+    }
     setValue(newValue);
   };
 
@@ -68,7 +78,7 @@ function Home() {
           component="img"
           alt="Artist"
           height='500'
-          image="https://images.unsplash.com/photo-1576328172036-6d52156c525f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=882&q=80"
+          image={header}
           title="Artist"
         />
         <CardContent>
@@ -85,8 +95,8 @@ function Home() {
             centered
             style={{ marginBottom: '-1.5em' }}>
             <Tab label="All time" />
-            <Tab label="Last Month" />
             <Tab label="Last 6 Months" />
+            <Tab label="Last Month" />
           </Tabs>
 
         </CardContent>
